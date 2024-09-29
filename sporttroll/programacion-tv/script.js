f5231b0c620159617f32eeb321df097b35d044bb
José Cortés <65973807+MikeTrollYT@users.noreply.github.com>
2024-09-29 12:46:48 +0200

    Add files via upload

--- sporttroll/programacion-tv/script.js
+++ sporttroll/programacion-tv/script.js
@@ -10,16 +10,20 @@ $(document).ready(function() {
     $('.title-section-widget').each(function() {
         const titleText = $(this).text().trim();
         const dateMatch = titleText.match(/(\d{1,2}) de (\w+) de (\d{4})/);
-        
+
         if (dateMatch) {
             const eventDay = parseInt(dateMatch[1], 10);
             const eventMonth = new Date(Date.parse(dateMatch[2] + " 1")).getMonth(); // Convertir el mes
             const eventYear = parseInt(dateMatch[3], 10);
             const eventDate = new Date(eventYear, eventMonth, eventDay); // Crear objeto Date para el evento
 
-            // Verificar si la fecha del evento es hoy
-            if (eventDate.toDateString() === now.toDateString()) {
-                // Iterar sobre los eventos de este día
+            // Verificar si la fecha del evento es anterior a hoy
+            if (eventDate < now.setHours(0, 0, 0, 0)) { // Si el evento es de un día anterior
+                $(this).hide(); // Ocultar el encabezado del día anterior
+                $(this).next('ol').find('.dailyevent').hide(); // Ocultar todos los eventos del día anterior
+            } else if (eventDate.toDateString() === now.toDateString()) {
+                // Iterar sobre los eventos del día de hoy
+                let allEventsHidden = true;
                 $(this).next('ol').find('.dailyevent').each(function() {
                     const eventHour = parseInt($(this).find('.dailyhour').text().trim().split(':')[0], 10);
                     const eventMinute = parseInt($(this).find('.dailyhour').text().trim().split(':')[1], 10);
@@ -28,8 +32,15 @@ $(document).ready(function() {
                     // Comprobar si el evento ya ha pasado y han pasado más de 3 horas
                     if (currentTimeInMinutes > eventTimeInMinutes + hoursToHide * 60) {
                         $(this).hide(); // Ocultar el evento
+                    } else {
+                        allEventsHidden = false; // Hay al menos un evento visible, no ocultar el encabezado
                     }
                 });
+
+                // Si todos los eventos del día de hoy están ocultos, también ocultar el encabezado
+                if (allEventsHidden) {
+                    $(this).hide(); // Ocultar el encabezado del día actual si todos los eventos están ocultos
+                }
             }
         }
     });
