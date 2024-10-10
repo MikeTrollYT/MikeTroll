db44cbd4dcb0568ebe69910a4c3e2c786c2ba30c
José Cortés <65973807+MikeTrollYT@users.noreply.github.com>
2024-10-10 17:56:44 +0200

    Update script.js

--- sporttroll/versiones/3.3/script.js
+++ sporttroll/versiones/3.3/script.js
@@ -1,83 +1,51 @@
-var html = document.getElementsByTagName("html")[0];
-var sun = document.querySelector(".sun");
-var rainbow = document.querySelector(".rainbow");
-var rabbit = document.querySelector(".rabbit");
-var snow = document.querySelectorAll(".snow");
-var rain = document.querySelectorAll(".rain");
-var flowers = document.querySelectorAll(".flower");
-var lightColours = ["#93d5eb", "#add63a", "#c5d63a", "#febe42"],
-mediumColours = ["#66bbd8", "#92c938", "#acc52b", "#ff9d25"],
-darkColours = ["#4da2bb", "#2a9d5c", "#89a503", "#ff6b2f"],
-backgroundColours = ["#cbe9f4", "#daf8ff", "#feec98", "#ffdc8a"],
-bushColours = ["#ffffff", "#3ebf6d", "#99b31a", "#fd6d2e"],
-cloudColours = ["#ffffff", "#ffffff", "#ffffff", "#eaf9fe"],
-seasons = ["Winter", "Spring", "Summer", "Autumn"];
-var c = 1;
-var season;
+var bubbles = document.getElementsByClassName("jetBubble");
+var mainTimeline = new TimelineMax({ repeat: -1 });
 
-function updateSeasons() {
-  html.style.setProperty("--bgd-color", backgroundColours[c]);
-  html.style.setProperty("--light", lightColours[c]);
-  html.style.setProperty("--medium", mediumColours[c]);
-  html.style.setProperty("--dark", darkColours[c]);
-  html.style.setProperty("--bush", bushColours[c]);
-  html.style.setProperty("--cloud", cloudColours[c]);
-  season = seasons[c];
-
-  //add snow if season = winter
-  snow.forEach(function (el) {
-    season !== seasons[0] ?
-    el.style.display = "none" :
-    el.style.display = "block";
+function createBubbles() {
+  TweenMax.set(bubbles, {
+    attr: {
+      r: "-=5"
+    }
   });
-
-  //add rabbit if season = winter
-  if (season === seasons[0] || season === seasons[2]) {
-    rabbit.classList.add("animated");
-  } else {
-    rabbit.classList.remove("animated");
+  //jet bubbles
+  for (var i = 0; i < bubbles.length; i++) {
+    var b = bubbles[i];
+    var tl = new TimelineMax({ repeat: -1 });
+    tl
+      .to(b, 1, {
+        attr: {
+          r: "+=5"
+        },
+        ease: Linear.easeNone
+      })
+      .to(b, 1, {
+        attr: {
+          r: "-=5"
+        },
+        ease: Linear.easeNone
+      });
+    mainTimeline.add(tl, i / 2);
   }
-
-  //add rainbow if season = spring
-  season === seasons[1] ?
-  rainbow.classList.add("animated") :
-  rainbow.classList.remove("animated");
-  if (season === seasons[1]) {
-    html.style.setProperty("--rabbit", "#9E6255");
-  }
-  //add flowers if season = spring
-  flowers.forEach(function (el) {
-    season === seasons[1] ?
-    el.classList.add("animated") :
-    el.classList.remove("animated");
-  });
-
-  //add sun and rabbit if season = summer
-  if (season === seasons[2]) {
-    html.style.setProperty("--sun", "#ffb53a");
-    html.style.setProperty("--rabbit", "#9E6255");
-  } else {
-    html.style.setProperty("--sun", "transparent");
-    html.style.setProperty("--rabbit", "#ffffff");
-  }
-
-  //add rain if season = autumn
-  rain.forEach(function (el) {
-    season === seasons[3] ?
-    el.style.display = "block" :
-    el.style.display = "none";
-  });
-
-  //increment array index
-  c = (c + 1) % seasons.length;
 }
+createBubbles();
 
-//animate all
-let nF = 0;
-function animate() {
-  if (++nF % 600 === 0) {
-    updateSeasons();
+window.onscroll = event => {
+  let topScroll = this.scrollY,
+    layers = document.querySelectorAll('[data-type="parallax"]'),
+    prefixes = [
+      "-webkit-transform",
+      "-moz-transform",
+      "-ms-transform",
+      "-o-transform"
+    ];
+
+  for (let layer of Array.prototype.slice.call(layers)) {
+    let depth = layer.getAttribute("data-depth"),
+      movement = -1 * (topScroll * depth),
+      translate = `translate3d(0, ${movement}px, 0)`;
+    for (let prefix of prefixes) {
+      layer.style[prefix] = translate;
+    }
+    layer.style.transform = translate;
   }
-  requestAnimationFrame(animate);
-}
-animate();
+};
